3eff62ada7e3be4ffcf965478291b7c87cdd95b7
hnkothar <130112699+hnkothar@users.noreply.github.com>
2024-12-10 01:21:18 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
 //source Code
+#include <WiFi.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <Adafruit_SSD1306.h>
+
+#define DHTPIN 15 // Pin connected to the DHT sensor
+#define DHTTYPE DHT11
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+DHT dht(DHTPIN, DHTTYPE);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+const char* ssid = "your-SSID";
+const char* password = "your-PASSWORD";
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+
+  if (!display.begin(SSD1306_I2C_ADDRESS, 0x3C)) {
+    Serial.println("SSD1306 allocation failed");
+    for (;;);
+  }
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+}
+
+void loop() {
+}
